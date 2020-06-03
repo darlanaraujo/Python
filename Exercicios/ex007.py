a2bab3e1bac2c2a18d140b616bd07a0b0f12700e
Darlan Araujo <63656231+darlanaraujo@users.noreply.github.com>
2020-06-03 10:11:10 -0300

    Create ex007.py

---
+++ Exercicios/ex007.py
@@ -0,0 +1,9 @@
+#Desenvolva um programa que leia as duas notas de um aluno
+#e calcule e mostre a sua média.
+print('{:=^40}'.format('EXERCICIO 007'))
+print('Vamos ver a média da sua nota!')
+n1 = float(input('Qual a sua primeira nota: '))
+n2 = float(input('Qual a sua segunda nota: '))
+s = (n1 + n2) / 2
+print('A sua média é {}'.format(s))
+print('{:=^40}'.format('='))
