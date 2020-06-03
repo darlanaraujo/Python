6f3f89efae0df9b85003a1959ed974f202bf2b80
Darlan Araujo <63656231+darlanaraujo@users.noreply.github.com>
2020-06-03 10:11:31 -0300

    Create ex016 - Aula 08.py

---
+++ Exercicios/ex016 - Aula 08.py
@@ -0,0 +1,11 @@
+'''
+DESAFIO 016
+
+Crie um programa  que LEIA UM NÚMERO REAL QUALQUER pelo teclado
+e MOSTRE NA TELA A SUA PORÇÃO INTEIRA.
+
+EX: Digite um número: 6.127
+O número 6.127 tem a parte inteira 6.
+
+***De uma olhada na classe de importação math
+'''
