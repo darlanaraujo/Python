268a9468fbe22e004b387a1bedc0ffc71c1e0ac5
Darlan Araujo <63656231+darlanaraujo@users.noreply.github.com>
2020-06-03 10:11:15 -0300

    Create ex009.py

---
+++ Exercicios/ex009.py
@@ -0,0 +1,17 @@
+#Faça um programa que leia um número inteiro e mostro na
+#tela a sua tabuada.
+print ('{:=^40}'.format ('EXERCICIO 009'))
+print ('Vamos conhecer a tabuada!')
+n1 = int (input ('Dígite um número: '))
+s1 = n1*1
+s2 = n1*2
+s3 = n1*3
+s4 = n1*4
+s5 = n1*5
+s6 = n1*6
+s7 = n1*7
+s8 = n1*8
+s9 = n1*9
+s10= n1*10
+print ('A tabuada de {} é: \n{} X 1 = {} \n{} X 2 = {} \n{} X 3 = {} \n{} X 4 = {} \n{} X 5 = {} \n{} X 6 = {} \n{} X 7 = {} \n{} X 8 = {} \n{} X 9 = {} \n{} X 10 = {}'.format (n1, n1, s1, n1, s2, n1, s3, n1, s4, n1, s5, n1, s6, n1, s7, n1, s8, n1, s9, n1, s10))
+print ('{:=^40}'.format ('='))
