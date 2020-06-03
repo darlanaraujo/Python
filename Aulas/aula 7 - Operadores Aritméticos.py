185194b2922f64f09a9b7bf3bcc2bbdaa977c69a
Darlan Araujo <63656231+darlanaraujo@users.noreply.github.com>
2020-06-03 10:07:04 -0300

    Create aula 7 - Operadores Aritméticos.py

---
+++ Aulas/aula 7 - Operadores Aritméticos.py
@@ -0,0 +1,9 @@
+n1 = int (input ('Digite um número: '))
+n2 = int (input ( 'Digite outro número: '))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+r = n1 % n2
+print ('A soma é: {}, a Multiplicação é: {}, e a Divisão fica: {}'.format (s,m,d))
+print ('A divisão inteira: {}, e o resto: {}'.format (di,r))
