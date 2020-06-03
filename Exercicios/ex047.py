3d5e9dbffdf65003ae351f1b6c85d035908c782e
Darlan Araujo <63656231+darlanaraujo@users.noreply.github.com>
2020-06-03 10:12:48 -0300

    Create ex047.py

---
+++ Exercicios/ex047.py
@@ -0,0 +1,9 @@
+'''
+DESAFIO 047
+
+Escreva um programa que mostre na tela TODOS OS NÚMEROS PARES
+que estão no intervalo entre 1 e 50.
+'''
+for contagem in range(2, 50 +1, 2):
+    print(contagem, end= ' ')
+print('Acabou')
