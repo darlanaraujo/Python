7480719bce86923f7eaf4f62f386265c49de3235
Darlan Araujo <63656231+darlanaraujo@users.noreply.github.com>
2020-06-03 10:11:26 -0300

    Create ex012.py

---
+++ Exercicios/ex012.py
@@ -0,0 +1,6 @@
+print ('{:=^40}'.format ('EXERCICIO 012'))
+print ('Desconto de 5% no valor do produto')
+n1 = float (input ('Qual o valor do produto? R$ '))
+s = (n1/100) * 95
+print ('O valor de R$ {} menos 5% fica R$ {:.2f}'.format (n1, s))
+print ('{:=^40}'.format ('='))
