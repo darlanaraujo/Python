52e98c02eb13b593abed76eadbbbeb46598b4140
Darlan Araujo <63656231+darlanaraujo@users.noreply.github.com>
2020-06-03 10:49:50 -0300

    Update ex090 - Aula 19.py
    Código basico solicitado no Exercício.

--- Exercicios/ex090 - Aula 19.py
+++ Exercicios/ex090 - Aula 19.py
@@ -4,3 +4,23 @@ DESAFIO 090 - AULA 19 DICIONÁRIOS
 Faça um programa que leia NOME E MÉDIA de um aluno, quardando também a SITUAÇÃO
 em um DICIONÁRIO. No final, mostre o conteúdo da estrutura na tela.
 '''
+# ============== MODELO CRIADO ===========================================================================
+relatorio = {'aprovado': 'Aprovado', 'recuperação': 'Recuperação', 'reprovado': 'Reprovado'}
+
+nome = str(input('Nome: '))
+media = float(input(f'Média do(a) {nome}: '))
+
+print('='*50)
+print(f'Aluno: {nome}')
+print(f'Média: {media}')
+if media <= 5:
+    print(f'Situação: {relatorio["reprovado"]}')
+elif media <= 6.9:
+    print(f'Situação: {relatorio["recuperação"]}')
+elif media >= 7:
+    print(f'Situação: {relatorio["aprovado"]}')
+# ============== MELHORIA DO CÓDIGO ======================================================================
+
+# ============== ANÁLISE DO CÓDIGO =======================================================================
+
+# ============== MODELO CURSO EM VIDEO ===================================================================
