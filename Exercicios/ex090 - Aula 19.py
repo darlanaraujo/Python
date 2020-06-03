a81c5930ebb1c356dd8cef2a432dfe3d972f8d1b
Darlan Araujo <63656231+darlanaraujo@users.noreply.github.com>
2020-06-03 10:14:31 -0300

    Create ex090 - Aula 19.py

---
+++ Exercicios/ex090 - Aula 19.py
@@ -0,0 +1,6 @@
+'''
+DESAFIO 090 - AULA 19 DICIONÁRIOS
+
+Faça um programa que leia NOME E MÉDIA de um aluno, quardando também a SITUAÇÃO
+em um DICIONÁRIO. No final, mostre o conteúdo da estrutura na tela.
+'''
