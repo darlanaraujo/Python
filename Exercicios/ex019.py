dcc8cb2f4d79a3ff9e9e7d8196bb091a1029ca5d
Darlan Araujo <63656231+darlanaraujo@users.noreply.github.com>
2020-06-03 10:11:38 -0300

    Create ex019.py

---
+++ Exercicios/ex019.py
@@ -0,0 +1,8 @@
+'''
+DESAFIO 019
+
+Um professor quer SORTEAR um dos seus QUATRO ALUNOS para apagar o quadro.
+
+Faça um programa que AJUDE ELE, LENDO O NOME DOS ALUNOS E ESCREVENDO O NOME
+DO ESCOLHIDO.
+'''
