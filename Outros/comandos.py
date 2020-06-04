a235899ee57ab2150ece3eb59c616a1056e18f96
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-03 21:05:47 -0300

    Create comandos.py

---
+++ Outros/comandos.py
@@ -0,0 +1,8 @@
+# Comando para definir quantidade de caracteres em um texto digitado, e a ordem de alinhamento;
+nome = input ('Olá qual o seu nome? ')
+print ('Prazer em te conhecer {:=>20}'.format (nome))
+#Ao colocar : e alguma informação dentro das {} ele replica a informação;
+# ^ Centraliza o texto;
+# > Alinha a partir da direita;
+# < Alinha a partir da esquerda;
+# '20' O numero é a definição da quantidade de caracteres que ele vai criar, incluindo o que for digitado;
