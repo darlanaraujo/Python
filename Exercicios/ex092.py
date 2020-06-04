b847ca40ef8cbf4aafe5426b84e8d239a845e958
Darlan Araujo <63656231+darlanaraujo@users.noreply.github.com>
2020-06-04 17:51:39 -0300

    Create ex092.py
    Descrição com o desafio.

---
+++ Exercicios/ex092.py
@@ -0,0 +1,8 @@
+'''
+DESAFIO 092
+
+Crie um programa que LEIA NOME, ANO DE NASCIMENTO e CARTEIRA DE TRABALHO e cadastre-os
+(COM IDADE) em um DICIONÁRIO se por acaso a CTPS for diferente de ZERO, o dicionário
+receberá também o ANO DE CONTRATAÇÃO e o SALÁRIO. Calcule e acrescente, além da
+IDADE, com quantos anos a pessoa vai se APOSENTAR.
+'''
