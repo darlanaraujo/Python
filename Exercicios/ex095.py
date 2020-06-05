cfdf1204968f78b76332342ecab0aae344fa40bb
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-05 17:29:32 -0300

    Update ex095.py
    Código base em construção. A primeira parte do código está pronta, já coletando os dados de diversos jogadores. Só falta construir os relatórios e apresentar de forma organizada as informações.

--- Exercicios/ex095.py
+++ Exercicios/ex095.py
@@ -5,6 +5,34 @@ Aprimore o DESAFIO 093 para que ele funcione com VÁRIOS JOGADORES, incluindo um
 DETALHES DO APROVEITAMENTO de cada JOGADOR.
 '''
 # ================= MODELO CRIADO =====================================================================================
+jogadores = {}
+gols = []
+dados = []
+cont = 0
+while True:
+    jogadores['nome'] = str(input('Nome do jogador: ')).title().strip()
+    partidas = int(input('Nº partidas: '))
+    jogadores['partidas'] = partidas
+    print('='*50)
+
+    for c in range(partidas):
+        n = int(input(f'==> Nº de gols na {c +1} partida: '))
+        cont += n
+        gols.append(n)
+
+    jogadores['gols'] = gols
+    jogadores['total'] = cont
+    dados.append(jogadores.copy())
+
+    print('='*50)
+    resp = str(input('Cadastrar outro jogador? [S/N]: '))
+    if resp in 'nN':
+        print('='*50)
+        break
+    print('='*50)
+
+for p, v in enumerate(dados):
+    print(f'Posição {p} | Jogador: {dados[p]["nome"]}  |  Gols: {dados[p]["total"]}')
 
 # ================= MELHORIA DO CÓDIGO ================================================================================
 
