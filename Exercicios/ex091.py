a73a23f087ef5e0a56e2ce7c699d08ae7f38b4a9
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-04 22:18:53 -0300

    Update ex091.py

--- Exercicios/ex091.py
+++ Exercicios/ex091.py
@@ -6,7 +6,7 @@ GUARDE esses RESULTADOS em um DICIONÁRIO. No final, coloque esse dicionário em
 ORDEM, sabendo que o VENCEDOR tirou o MAIOR NÚMERO no DADO.
 '''
 # ================ MODELO CRIADO ========================================================================
-'''from random import randint
+from random import randint
 from time import sleep
 
 jogadores = {'Darlan': randint(1, 6), 'Deivid': randint(1, 6),
@@ -16,20 +16,22 @@ jogadores = {'Darlan': randint(1, 6), 'Deivid': randint(1, 6),
 print('Valores sorteados:')
 for item in jogadores:
     print(f'    O jogador {item} tirou {jogadores[item]}')
-    sleep(.5)
+    sleep(.7)
 print('='*40)
-sleep(1)
+sleep(1.5)
+
 print('Colocação dos jogadores:')
+sleep(1)
 cont = 1
 for c in range(1):
     for item in sorted(jogadores, reverse=True, key=jogadores.get):
-        print(f'    em {cont}ª lugar {item}')
+        print(f'    {cont}ª lugar {item} com {jogadores[item]}')
         sleep(1)
         cont += 1
-'''
-# ================ MELHORIA DO CÓDIGO ===================================================================
-'''from random import randint
-from time import sleep
+
+# ================ MELHORIA DO CÓDIGO ===================================================================from random import randint
+'''from time import sleep
+from random import randint
 
 jogadores = {}
 
