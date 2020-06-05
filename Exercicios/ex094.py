89e0472895818082a2ee3e62c4ac1a2d27b4eadd
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-04 22:23:00 -0300

    Create ex094.py
    Anunciado do desafio 094

---
+++ Exercicios/ex094.py
@@ -0,0 +1,18 @@
+'''
+DESAFIO 094
+
+Crie um programa que LEIA NOME, SEXO e IDADE de VÁRIAS PESSOAS, guardando os dados de cada pessoa em um
+DICIONÁRIO e todos os dicionário em uma LISTA. No final, mostre:
+
+A) QUANTAS PESSOAS FORAM CADASTRADAS;
+B) A MÉDIA DE IDADE DO GRUPO;
+C) UMA LISTA COM TODAS AS MULHERES;
+D) UMA LISTA COM TODAS AS PESSOAS COM IDADE ACIMA DA MÉDIA.
+'''
+# ================= MODELO CRIADO =====================================================================================
+
+# ================= MELHORIA DO CÓDIGO ================================================================================
+
+# ================= ANAÁLISE DO CÓDIGO ================================================================================
+
+# ================= MODELO CURSO EM VIDEO =============================================================================
