13cc1d64aa72ef3300e9679e9c2a50cdf1e6ae72
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-05 14:54:42 -0300

    Update ex094.py
    Código base concluido. A primeira parte do código foi bem simples. Os dois últimos objetivos que deu mais trabalho. Fazer a varredura dentro da lista e separar apenas um item do dicionário. Usei a função enumerate para isso com a função IF. Após resolver o primeiro foi só fazer o mesmo com o outro.

--- Exercicios/ex094.py
+++ Exercicios/ex094.py
@@ -10,6 +10,38 @@ C) UMA LISTA COM TODAS AS MULHERES;
 D) UMA LISTA COM TODAS AS PESSOAS COM IDADE ACIMA DA MÉDIA.
 '''
 # ================= MODELO CRIADO =====================================================================================
+pessoas = {}
+dados = []
+media = soma = 0
+while True:
+    pessoas['nome'] = str(input('Nome: ')).title().strip()
+    pessoas['idade'] = int(input('Idade: '))
+    soma += pessoas['idade']
+    pessoas['sexo'] = str(input('Sexo [M/F]: ')).upper().strip()[0]
+    dados.append(pessoas.copy())
+    print('='*50)
+
+    resp = str(input('Continuar cadastrando? [S/N]: '))
+    if resp in 'nN':
+        break
+    print('='*50)
+
+media = soma / len(dados)
+print('='*50)
+print(f'{"RELATÓRIO DOS DADOS":^50}')
+print('='*50)
+print(f'A) Foram cadastradas {len(dados)} pessoas')
+print(f'B) A média de idade do grupo: {media:.1f} anos')
+print(f'C) Lista das mulheres cadastradas: ', end= '')
+
+for p, d in enumerate(dados):
+    if dados[p]['sexo'] == 'F':
+        print(f'{dados[p]["nome"]}', end=' ')
+print('\nD) Pessoas com idade acima da média: ', end='')
+for p, d in enumerate(dados):
+    if dados[p]['idade'] > media:
+        print(f'{dados[p]["nome"]}', end=' ')
+
 
 # ================= MELHORIA DO CÓDIGO ================================================================================
 
