71fde4e4326d4d5bd9e11be74a3ee0c72a4f862d
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-05 23:23:31 -0300

    Código ainda em desenvolvimento. estou finalizando a terceira parte, que traz o relatório individual de cada jogador pela posição. Só falta mostrar os gols por partida.

--- Exercicios/ex095.py
+++ Exercicios/ex095.py
@@ -8,19 +8,21 @@ DETALHES DO APROVEITAMENTO de cada JOGADOR.
 jogadores = {}
 gols = []
 dados = []
-cont = 0
+
 while True:
     jogadores['nome'] = str(input('Nome do jogador: ')).title().strip()
     partidas = int(input('Nº partidas: '))
     jogadores['partidas'] = partidas
     print('='*50)
+    cont = 0
 
     for c in range(partidas):
         n = int(input(f'==> Nº de gols na {c +1} partida: '))
         cont += n
         gols.append(n)
 
-    jogadores['gols'] = gols
+    jogadores['gols'] = gols[:]
+    gols.clear()
     jogadores['total'] = cont
     dados.append(jogadores.copy())
 
@@ -32,7 +34,16 @@ while True:
     print('='*50)
 
 for p, v in enumerate(dados):
-    print(f'Posição {p} | Jogador: {dados[p]["nome"]}  |  Gols: {dados[p]["total"]}')
+    print(f'Posição {p +1:<2} | Jogador: {dados[p]["nome"]:<8}  |  Gols: {dados[p]["total"]}')
+
+while True:
+    cons = int(input('Digite a posição para ver os dados: '))
+    for p, v in enumerate(dados):
+        if cons == p +1:
+            print(f'Jogador: {dados[p]["nome"]}')
+            print(f'Nª Partidas: {dados[p]["partidas"]}')
+            print(f'Gols: {dados[p]["gols"]}')
+            print(f'Total de Gols: {dados[p]["total"]}')
 
 # ================= MELHORIA DO CÓDIGO ================================================================================
 
