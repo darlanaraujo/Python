0cafbb2e27e3d55f24f87766de71faca6cfa6521
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-09 17:05:26 -0300

    Enunciado do desafio 100

---
+++ Exercicios/ex100.py
@@ -0,0 +1,15 @@
+'''
+DESAFIO 100
+
+Faça um programa que tenha uma LISTA chamada NÚMEROS e duas FUNÇÕES chamadas
+SORTEIA() e SOMAPAR(). A primeira função vai sortear 5 NÚMEROS e vai colocá-los
+dentro da lista e a segunda função vai mostrar a SOMA entre todos os valores
+PARES sorteados pela função anterior.
+'''
+# ================= MODELO CRIADO =====================================================================================
+
+# ================= MELHORIA DO CÓDIGO ================================================================================
+
+# ================= ANAÁLISE DO CÓDIGO ================================================================================
+
+# ================= MODELO CURSO EM VIDEO =============================================================================
