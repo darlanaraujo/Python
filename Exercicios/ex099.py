4fce24e04999619f870a8926a7ad17cd2af95264
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-09 17:05:18 -0300

    Enunciado do desafio 099

---
+++ Exercicios/ex099.py
@@ -0,0 +1,15 @@
+'''
+DESAFIO 099
+
+Faça um programa que tenha uma FUNÇÃO chamada MAIOR(), que receba vários PARÂMETROS
+com valores inteiros.
+
+Seu programa tem que ANALISAR todos os VALORES e dizer qual deles É O MAIOR.
+'''
+# ================= MODELO CRIADO =====================================================================================
+
+# ================= MELHORIA DO CÓDIGO ================================================================================
+
+# ================= ANAÁLISE DO CÓDIGO ================================================================================
+
+# ================= MODELO CURSO EM VIDEO =============================================================================
