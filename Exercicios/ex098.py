9313993ebddf840272772f7b7828ef6e187386bc
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-09 17:05:12 -0300

    Enunciado do desafio 098

---
+++ Exercicios/ex098.py
@@ -0,0 +1,18 @@
+'''
+DESAFIO 098
+
+Faça um programa que tenha uma FUNÇÃO chamada CONTADOR(), que receba três PARÂMENTROS:
+INÍCIO, FIM e PASSO e realize a contagem
+
+Seu programa tem que realizar TRÊS CONTAGENS através da função criada:
+A) DE 1 ATÉ 10, DE 1 EM 1
+B) DE 10 ATÉ 0, DE 2 EM 2
+C) UMA CONTAGEM PERSONALIZADA.
+'''
+# ================= MODELO CRIADO =====================================================================================
+
+# ================= MELHORIA DO CÓDIGO ================================================================================
+
+# ================= ANAÁLISE DO CÓDIGO ================================================================================
+
+# ================= MODELO CURSO EM VIDEO =============================================================================
