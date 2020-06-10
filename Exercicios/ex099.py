b058a7a266828ffeea258650aa0bf115735653df
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-10 17:16:50 -0300

    Update ex099.py
    O código base foi criado. Funcionando conforme o enunciado. Ainda usei um recurso diferente do pedido, já que possibilitei para que o usuário digite os valores.

--- Exercicios/ex099.py
+++ Exercicios/ex099.py
@@ -7,7 +7,24 @@ com valores inteiros.
 Seu programa tem que ANALISAR todos os VALORES e dizer qual deles É O MAIOR.
 '''
 # ================= MODELO CRIADO =====================================================================================
+from time import sleep
+num = []
+def maior(* num):
+    print('Analisando os valores: ', end='')
+    for valor in num:
+        print(valor, end=' ')
+        sleep(.5)
+    print()
+    print(f'{len(num)} números foram informado. O maior valor é {max(num)}')
 
+while True:
+    num.append(int(input('Digite um valor: ')))
+    print('='*40)
+    resp = str(input('Deseja adicionar outro? [S/N]: '))
+    if resp in 'nN':
+        break
+print('='*40)
+maior(* num)
 # ================= MELHORIA DO CÓDIGO ================================================================================
 
 # ================= ANAÁLISE DO CÓDIGO ================================================================================
