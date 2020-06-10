db9351a04436cf581827407549a55c29dbf96c56
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-10 17:16:18 -0300

    Update ex098.py
    O código base foi criado. Funcionando parcial conforme o enunciado. Ao colocar um valor positivo ou negativo isso aparece na resolução.

--- Exercicios/ex098.py
+++ Exercicios/ex098.py
@@ -10,7 +10,24 @@ B) DE 10 ATÉ 0, DE 2 EM 2
 C) UMA CONTAGEM PERSONALIZADA.
 '''
 # ================= MODELO CRIADO =====================================================================================
+from time import sleep
+def contador(a, b, c):
+    print('=' * 50)
+    print(f'Contatem de {a} até {b} de {c} em {c}:')
+    for c in range(a, b, c):
+        print(c, end=' ')
+        sleep(.5)
+    print('Fim!')
 
+
+contador(1, 10, 1)
+contador(10, 0, -2)
+print('='*50)
+print('Contagem personalizada. Defina a sua escolha')
+a = int(input('Defina o Início: '))
+b = int(input('Defina o Fim: '))
+c = int(input('Defina o intervalo: '))
+contador(a, b, c)
 # ================= MELHORIA DO CÓDIGO ================================================================================
 
 # ================= ANAÁLISE DO CÓDIGO ================================================================================
