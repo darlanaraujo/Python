5c54d3da74725ff92d7d6068a4b4e391b1b3699b
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-10 17:12:25 -0300

    Update ex096 - Aula 20.py
    O código base foi criado. Funcionando conforme o enunciado.

--- Exercicios/ex096 - Aula 20.py
+++ Exercicios/ex096 - Aula 20.py
@@ -5,7 +5,21 @@ Faça um programa que tenha uma FUNÇÃO chamada ÁREA(), que receba as dimensõ
 (LARGURA e COMPRIMENTO) e mostre a ÁREA DO TERRENO.
 '''
 # ================= MODELO CRIADO =====================================================================================
+def area(a, b):
+    s = a * b
+    print(f'{a}mt de Largura X {b}mt de Comprimento')
+    print('='*50)
+    print(f'ÁREA TOTAL DO TERRENO {s}M²')
 
+
+print('='*50)
+print(f'{"ÁREA DO TERRENO":^50}')
+print('='*50)
+
+a = float(input('Largura do terreno: '))
+b = float(input('Comprimento do terreno: '))
+print('='*50)
+area(a, b)
 # ================= MELHORIA DO CÓDIGO ================================================================================
 
 # ================= ANAÁLISE DO CÓDIGO ================================================================================
