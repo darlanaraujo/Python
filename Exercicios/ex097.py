fd6d95623051239b36d97c0304afb2edb02135db
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-10 17:15:33 -0300

    Update ex097.py
    O código base foi criado. Funcionando conforme o enunciado. Para colocar o texto centralizado e pelo menos 2 casas menor que a linha só foi possível dando espaço ao digitar. Não conseguir fazer de um jeito automatizado.

--- Exercicios/ex097.py
+++ Exercicios/ex097.py
@@ -5,13 +5,20 @@ Faça um programa que tenha um FUNÇÃO chamada ESCREVA(), que receba um texto q
 PARÂMETRO e mostre uma mensagem com tamanho adaptável.
 
 Ex: escreva('Olá, Mundo!')
-
 Saída:
 ---------------
   Olá, Mundo!
 ---------------
 '''
 # ================= MODELO CRIADO =====================================================================================
+def escreva(txt):
+    print('-' * len(txt))
+    print(f'{txt}')
+    print('-' * len(txt))
+
+txt = str(input('Escolha o titulo: '))
+
+escreva(txt)
 
 # ================= MELHORIA DO CÓDIGO ================================================================================
 
