aa06e5db3aa7d7d853689594d7f55dfeb2bc5f24
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-10 23:24:03 -0300

    Código base concluido. Funcionando conforme o enunciado.

--- Exercicios/ex100.py
+++ Exercicios/ex100.py
@@ -7,6 +7,29 @@ dentro da lista e a segunda função vai mostrar a SOMA entre todos os valores
 PARES sorteados pela função anterior.
 '''
 # ================= MODELO CRIADO =====================================================================================
+from random import randint
+from time import sleep
+numeros = []
+
+def sorteia():
+    for c in range(1, 6):
+        a = randint(0, 10)
+        numeros.append(a)
+    somapar()
+
+def somapar():
+    soma = 0
+    print(f'Sorteando os números >>> ', end='')
+    for p, v in enumerate(numeros):
+        if v % 2 == 0:
+            soma += v
+        sleep(.5)
+        print(f'{v}', end=' ')
+    print('Fim!')
+    sleep(1)
+    print(f'A soma dos valores pares foi {soma}')
+
+sorteia()
 
 # ================= MELHORIA DO CÓDIGO ================================================================================
 
