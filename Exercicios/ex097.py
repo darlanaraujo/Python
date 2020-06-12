87fdde39bcded2a3f9f6b3af16679ff9aab0a962
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-11 22:25:51 -0300

    Update ex097.py
    Código base do curso inserido. Na análise do código descrevo a diferença entre os dois.

--- Exercicios/ex097.py
+++ Exercicios/ex097.py
@@ -11,17 +11,31 @@ Saída:
 ---------------
 '''
 # ================= MODELO CRIADO =====================================================================================
-def escreva(txt):
+'''def escreva(txt):
     print('-' * len(txt))
     print(f'{txt}')
     print('-' * len(txt))
 
+
 txt = str(input('Escolha o titulo: '))
-
-escreva(txt)
-
+escreva(txt)'''
 # ================= MELHORIA DO CÓDIGO ================================================================================
 
 # ================= ANAÁLISE DO CÓDIGO ================================================================================
-
+'''
+No meu código ao usar a função len sem criar uma variável ficaria mais elegante uma vez que usei direto no print.
+Porém ela não resolve a inclusão de mais 4 simbolos para deixar o texto centralizado. Só funciona se a pessoa digitar
+o espaço ao criar o titulo. Por esse motivo o código do curso é mais funional.
+'''
 # ================= MODELO CURSO EM VIDEO =============================================================================
+def escreva(msg):
+    tam = len(msg) + 4
+    print('~' * tam)
+    print(f'  {msg}')
+    print('~' * tam)
+
+
+#Programa Principal
+escreva('Gustavo Guanabara')
+escreva('Curso de Python no YouTube')
+escreva('CeV')
