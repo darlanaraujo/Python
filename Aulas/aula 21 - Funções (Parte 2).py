bb914ff76c3e4b0c1844d171cf7081b37aa6514a
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-19 17:38:39 -0300

    Create aula 21 - Funções (Parte 2).py
    Aula 21 - Funções (parte 2)
    Com descrições da minha visão sobre as funções.

---
+++ Aulas/aula 21 - Funções (Parte 2).py
@@ -0,0 +1,32 @@
+'''
+AULA 21 - FUNÇÕES (PARTE 2)
+
+>> INTERACTIVE HELP:
+Ajuda interativa, é um recurso de documentação para consulta de todas as funções da linguagem.
+A forma de acesso é com a função help()
+Você pode 
+
+>> DOCSTRINGS:
+
+>> ARGUMENTOS OPCIONAIS:
+
+>> ESCOPO DE VARIÁVEIS:
+
+>> RETORNO DE RESULTADOS:
+
+'''
+def linha(txt):
+    print()
+    print(f'{txt:^60}')
+    print('='*60)
+
+# ================ INTERACTIVE HELP ===================================================================================
+linha('INTERACTIVE HELP')
+
+# ================ DOCSTRINGS =========================================================================================
+
+# ================ ARGUMENTOS OPCIONAIS ===============================================================================
+
+# ================ ESCOPO DE VARIÁVEIS ================================================================================
+
+# ================ RETORNO DE RESULTADOS ==============================================================================
