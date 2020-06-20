adcc4c8238f187dc53175c9a2e40ae2c72108b55
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-20 00:38:32 -0300

    Update teste 2.py

--- Outros/teste 2.py
+++ Outros/teste 2.py
@@ -1,13 +1,38 @@
-dados = []
-jogador = {'jogador': 'Darlan', 'partida': 2, 'gols': [1, 0], 'total': 2, 'media': 0.5}
-dados.append(jogador.copy())
-jogador.clear()
+def teste(num):
+    global a, b, c #Ao colocar essa função, as variáveis locais passaram ser globais
+    a = 8
+    b = num + 4 #NUM é o valor de A Global
+    c = 2
+    print(f'No programa Local, A vale {a}')
+    print(f'No programa Local, B vale {b}')
+    print(f'No programa local, C vale {c}')
 
-jogador = {'jogador': 'Davi', 'partida': 2, 'gols': [3, 2], 'total': 5, 'media': 2.5}
-dados.append(jogador.copy())
+print('PROGRAMA GLOBAL')
+print('-' *30)
+a = b = c = 0
+a = 5
+print(f'No programa Global, A vale {a}')
+print(f'No programa Global, B vale {b}')
+print(f'No programa Global, C vale {c}')
 
-for p, v in enumerate(dados):
-    print(f'Na posição: {p}, encontrei {v}')
-for k, v in jogador.items():
-    print(f'Na chave {k} encontrei {v}')
+print()
+
+print('PROGRAMA LOCAL')
+print('-' *30)
+teste(a) #O valor de A foi passado para o parâmetro NUM
+print()
+print('NA SEGUNDA CHAMADA OS RESULTADOS MUDAM')
+print('=' *30)
+print(f'No programa Global, A vale {a}')
+print(f'No programa Global, B vale {b}')
+print(f'No programa Global, C vale {c}')
+
+print()
+
+print('PROGRAMA LOCAL')
+print('-' *30)
+teste(a)
+
+
+
 
