145568165e3293a644c806ee09abe1cec27f7571
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-21 20:41:25 -0300

    Create ex104.py
    Enunciado do exercício 104

---
+++ Exercicios/ex104.py
@@ -0,0 +1,8 @@
+'''
+DESAFIO 104
+
+Crie um programa que tenha a FUNÇÃO LEIAINT(), que vai funcioinar de forma semelhante à função INPUT() do Python, só que
+fazendo a VALIDAÇÃO para aceitar apenas um valor numérico.
+
+Ex: n=leiaint(''igite um número: ')
+'''
