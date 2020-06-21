8c368af7b1d70b79303ae2669042060d79cc6503
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-21 20:41:33 -0300

    Create ex105.py
    Enunciado do exercício 105

---
+++ Exercicios/ex105.py
@@ -0,0 +1,14 @@
+'''
+DESAFIO 105
+
+Faça um programa que tenha uma FUNÇÃO NOTAS() que pode receber várias notas de alunos e vai retornar um DICIONÁRIO
+com as seguintes informações:
+
+- Quantidade de notas
+- A maior nota
+- A menor nota
+- A média da turma
+- A situação (opcional)
+
+Adicione também as DOCSTRINGS da função.
+'''
