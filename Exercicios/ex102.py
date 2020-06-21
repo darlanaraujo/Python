41ac6257f96cf2a41f4c29229ca0e80a4d4338bf
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-21 20:41:09 -0300

    Create ex102.py
    Enunciado do exercício 102

---
+++ Exercicios/ex102.py
@@ -0,0 +1,12 @@
+'''
+DESAFIO 102
+
+Crie um programa que tenha uma FUNÇÃO FATORIAL() que receba dois parâmetros: O Primeiro que indique o NÚMERO a calcular
+e o outro chamado SHOW, que será um valor LÓGICO (OPCIONAL) indicando se será mostrado ou não na tela o processo de
+cálculo do fatorial.
+
+fatorial(n, show=False)
+parâmetro n: O número a ser calculado
+parâmetro show: (opcional) mostra ou não um número n
+return: O valor do fatorial de um número n
+'''
