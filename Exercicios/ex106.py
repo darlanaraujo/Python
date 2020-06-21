d45aec36c7065f8c40262f929307f388c3dd7f43
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-21 20:41:40 -0300

    Create ex106.py
    Enunciado do exercício 106

---
+++ Exercicios/ex106.py
@@ -0,0 +1,8 @@
+'''
+DESAFIO 106
+
+Faça um MINI-SISTEMA que utilize o INTERACTIVE HELP do PYTHON. O usuário vai digitar o COMANDO e o MANUAL vai aparecer.
+Quando o usuário digitar a palavra FIM, o programa se ENCERRARÁ.
+
+Obs: Use CORES.
+'''
