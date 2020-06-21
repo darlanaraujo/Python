9adc522ce48a2b8c805abb36b32f4136d29350c4
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-21 20:41:18 -0300

    Create ex103.py
    Enunciado do exercício 103

---
+++ Exercicios/ex103.py
@@ -0,0 +1,8 @@
+'''
+DESAFIO 103
+
+Faça um programa que tenha uma FUNÇÃO chamada FICHA(), que receba dois PARÂMETROS OPCIONAIS: O NOME de um jogador e
+quantos GOLS ele marcou.
+
+O programa deverá ser capaz de mostrar a FICHA DO JOGADOR, mesmo que algum dado não tenha sido informado corretamente.
+'''
