c935d9e3f89783ff128446ab5a8d71cf07fe56e9
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-22 19:05:03 -0300

    Update ex103.py
    Código base concluido. Programa funciona conforme pedido do enunciado. A única dúvida que fiquei é que o código só da certo se a variável gols ficar como string, se ficar como número o programa da erro.

--- Exercicios/ex103.py
+++ Exercicios/ex103.py
@@ -6,3 +6,31 @@ quantos GOLS ele marcou.
 
 O programa deverá ser capaz de mostrar a FICHA DO JOGADOR, mesmo que algum dado não tenha sido informado corretamente.
 '''
+# ================ CÓDIGO BASE ========================================================================================
+def ficha(n='<Desconhecido>', g=0):
+    '''
+        --> A função ficha recebe a informação do usuário com o nome do jogador e quantos gols ele fez. Caso algum
+        parâmetro não seja preenchido, ele sera substituido pelo parâmetro estipulado.
+    :param n: Nome do jogador ou caso não seja informado recebe o texto pré-definido pelo programa.
+    :param g: Número de gols ou caso não seja informado recebe 0.
+    :return: Mostra na tela os dados do jogador.
+    '''
+    print(f'O jogador {n} fez {g} gols.')
+
+jogador = str(input('Nome do jogador: ')).title().strip()
+gols = str(input('Número de gols: '))
+
+if jogador == '' and gols == '':
+    ficha()
+elif jogador == '':
+    ficha(g=gols)
+elif gols == '':
+    ficha(n=jogador)
+else:
+    ficha(jogador, gols)
+
+# ================ CÓDIGO MELHORADO ===================================================================================
+
+# ================ ANÁLISE DO CÓDIGO ==================================================================================
+
+# ================ CÓDIGO CURSO EM VÍDEO ==============================================================================
