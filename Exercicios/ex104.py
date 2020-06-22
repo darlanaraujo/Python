1eb1ed0238a507467a2ec3e2c0200bd08ee93c97
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-22 19:06:40 -0300

    Update ex104.py
    Código base concluido. Programa funcionando conforme o enunciado. Apesar de funcionar, tenho dúvida se o meu código vai ficar igual ao do curso. Uma vez que a variável n no programa principal não tem utilidade, se colocasse a chamada da função sem a variável funcionaria do mesmo modo.

--- Exercicios/ex104.py
+++ Exercicios/ex104.py
@@ -6,3 +6,21 @@ fazendo a VALIDAÇÃO para aceitar apenas um valor numérico.
 
 Ex: n=leiaint(''igite um número: ')
 '''
+# ============= CÓDIGO BASE ===========================================================================================
+def leiaint(num):
+    while True:
+        global n
+        n = input(num)
+        if n.isnumeric() == True:
+            break
+        else:
+            print('ERRO! Digite apenas números.')
+
+
+n = leiaint('Digite um número: ')
+print(f'Você acabou de digitar o número {n}')
+# ============= CÓDIGO MELHORADO ======================================================================================
+
+# ============= ANÁLISE DO CÓDIGO =====================================================================================
+
+# ============= CÓDIGO CURSO EM VÍDEO =================================================================================
