a0b80babf90d2841236c4504dd2f0a6d76c84623
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-24 17:58:11 -0300

    Update ex104.py
    Código do curso inserido.

--- Exercicios/ex104.py
+++ Exercicios/ex104.py
@@ -7,12 +7,12 @@ fazendo a VALIDAÇÃO para aceitar apenas um valor numérico.
 Ex: n=leiaint(''igite um número: ')
 '''
 # ============= CÓDIGO BASE ===========================================================================================
-def leiaint(num):
-    '''
+'''def leiaint(num):
+    
         --> Função que funciona como o input, lendo uma informação iserida pelo usuário.
     :param num: Valor inserido pelo usuário.
     :return: Mostra o valor que o usuário digitou.
-    '''
+    
     while True:
         global n
         n = input(num)
@@ -23,11 +23,27 @@ def leiaint(num):
 
 
 n = leiaint('Digite um número: ')
-print(f'Você acabou de digitar o número {n}')
+print(f'Você acabou de digitar o número {n}')'''
 # ============= CÓDIGO MELHORADO ======================================================================================
 
 # ============= ANÁLISE DO CÓDIGO =====================================================================================
 
 # ============= CÓDIGO CURSO EM VÍDEO =================================================================================
+def leiaint(msg): # A msg é o texto 'Digite um número'
+    ok = False # Essa variável é apenas para iniciar ou parar o laço.
+    valor = 0
+    while True:
+        n = str(input(msg)) # Aqui ele inseriu a mensagem na variavel n como se fosse n=str(input('Digite um número'))
+        if n.isnumeric(): # Aqui ele vai verificar se n é um número.
+            valor = int(n) # Sendo um número, n recebe o valor que foi digitado em n
+            ok = True
+        else:
+            print('\033[0;31mERRO! Digite um número inteiro válido.\033[m')
+        if ok:
+            break
+    return valor # Sem esse retorno, o valor que foi inserido no laço não é validado.
 
 
+# Programa Principal
+n = leiaint('Digite um número: ')
+print(f'Você acabou de digitar o número {n}')
