ae6d9acfc575135f8fc66862947d1c30657dc289
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-24 17:27:44 -0300

    Update ex103.py
    Código do curso inserido

--- Exercicios/ex103.py
+++ Exercicios/ex103.py
@@ -34,3 +34,18 @@ else:
 # ================ ANÁLISE DO CÓDIGO ==================================================================================
 
 # ================ CÓDIGO CURSO EM VÍDEO ==============================================================================
+'''def ficha(jog='<Desconhecido>', gol=0):
+    print(f'O jogador {jog} fez {gol} gol(s) no campeonato')
+
+
+# Programa principal
+n = str(input('Nome do Jogador: '))
+g = str(input('Número de Gols: '))
+if g.isnumeric():
+    g = int(g)
+else:
+    g = 0
+if n.strip() == '':
+    ficha(gol=g)
+else:
+    ficha(n, g)'''
