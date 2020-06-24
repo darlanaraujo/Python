44b1c876842d19ee4d2e72732cd44184854dbcfe
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-24 15:52:45 -0300

    Update ex101 - Aula 21.py
    Código do curso inserido.

--- Exercicios/ex101 - Aula 21.py
+++ Exercicios/ex101 - Aula 21.py
@@ -17,22 +17,41 @@ def voto(ano):
     hoje = date.today().year
     idade = hoje - ano
     print(f'Com {idade} anos: ', end='')
-    if idade < 15:
-        print('NÃO VOTA!')
+    if idade < 16:
+        #print('NÃO VOTA!')
         return 'NÃO VOTA'
     elif idade == 16 or idade < 18 or idade >= 65:
-        print('VOTO OPCIONAL.')
+        #print('VOTO OPCIONAL.')
         return 'VOTO OPCIONAL'
     else:
-        print('VOTO OBRIGATÓRIO.')
+        #print('VOTO OBRIGATÓRIO.')
         return 'VOTO OBRIGATÓRIO'
 
 help(voto)
 ano = int(input('Em que ano você nasceu? '))
+#IMPORTANTE! Para usar o retorno da função para ser mostrado, só funiona se abaixo a chamada da função estiver no print.
+print(voto(ano))
 voto(ano)
     
 # ================== CÓDIGO MELHORADO =================================================================================
 
 # ================== ANÁLISE DO CÓDIGO ================================================================================
+'''
 
+'''
 # ================== CÓDIGO CURSO EM VÍDEO ============================================================================
+def voto(ano):
+    from datetime import date
+    atual = date.today().year
+    idade = atual - ano
+    if idade < 16:
+        return f'Com {idade} anos: NÃO VOTA.'
+    elif 16 <= idade < 18 or idade > 65:
+        return f'Com {idade} anos: VOTO OPCIONAL.'
+    else:
+        return f'Com {idade} anos: VOTO OBRIGATÓRIO.'
+
+
+# Programa principal
+nasc = int(input('Em que ano você nasceu? '))
+print(voto(nasc))
