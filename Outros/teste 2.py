37d3e72cdc68da0a435171f7a1d40c546d0c98cc
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-24 20:24:39 -0300

    Update teste 2.py

--- Outros/teste 2.py
+++ Outros/teste 2.py
@@ -1,15 +1,9 @@
-time = ('santos', 'vasco', 'flamengo')
+jogador = {'jogador': 'Darlan', 'partida': 1}
 
-print(time.count('santos'))
-num = int(input('Qual a posição da tabela: '))
-print(len(time[num]))
-'''while True:
-    num = int(input('Qual a posição da tabela: '))
-    print(time[num])
+jogador['partida'] += 2
 
-    resp = str(input('Deseja consultar outro time? [S/N]: ')).upper().strip()[0]
-    if resp == 'S':
-        print()
-    else:
-        print('Fim do programa!')
-        break'''
+print(jogador)
+
+jogador['partida'] += 1
+
+print(jogador)
