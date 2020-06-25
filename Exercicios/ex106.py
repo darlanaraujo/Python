a315c6866a184e8d10e37c2b29b4a08f64e70a68
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-25 16:17:52 -0300

    Update ex106.py
    Código do curso inserido. Tem vários recursos bem interessantes. A organização das cores utilizando uma lista e a criação de uma função para criar um titulo personalizado.

--- Exercicios/ex106.py
+++ Exercicios/ex106.py
@@ -7,7 +7,7 @@ Quando o usuário digitar a palavra FIM, o programa se ENCERRARÁ.
 Obs: Use CORES.
 '''
 # ============ CÓDIGO BASE ============================================================================================
-def ajuda(txt):
+'''def ajuda(txt):
     """
         --> A função recebe um parâmetro de texto que é usado para interação com o usuário. Não digitar a palavra que
         deseja saber o manual a função coloca essa palavra dentro da função help e devolve o conteúdo.
@@ -42,9 +42,46 @@ def ajuda(txt):
 
 
 #Programa principal
-ajuda('Digite a Função ou Comando: >> ')
+ajuda('Digite a Função ou Comando: >> ')'''
 # ============ CÓDIGO MELHORADO =======================================================================================
 
 # ============ ANÁLISE DO CÓDIGO ======================================================================================
 
 # ============ CÓDIGO DO CURSO EM VÍDEO ===============================================================================
+from time import sleep
+c= ('\033[m',           # 0 - sem cores
+    '\033[0;30;41m',    # 1 - vermelho
+    '\033[0;30;42m',    # 2 - verde
+    '\033[0;30;43m',    # 3 - amarelo
+    '\033[0;30;44m',    # 4 - azul
+    '\033[0;30;45m',    # 5 - roxo
+    '\033[7;30m'        # 6 - branco
+    )
+def ajuda(com):
+    titulo(f'ACESSANDO O MANUAL DO COMANDO \'{com.upper()}\'', 4)
+    print(c[6], end='')
+    help(com)
+    print(c[0], end='')
+    sleep(2)
+
+
+def titulo(msg, cor=0):
+    tam = len(msg) + 4
+    print(c[cor], end='')
+    print('~' * tam)
+    print(f'  {msg}')
+    print('~' * tam)
+    print(c[0], end='')
+    sleep(1)
+
+
+# Programa principal
+comando = ''
+while True:
+    titulo('SISTEMA DE AJUDA PYHELP', 2)
+    comando = str(input('Função ou Biblíoteca > '))
+    if comando.upper() == 'FIM':
+        break
+    else:
+        ajuda(comando)
+titulo('ATÉ LOGO!', 1)
