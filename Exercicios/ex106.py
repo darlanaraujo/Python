fee063aef3c5eaafab121c1689ea20cf32c92e83
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-25 15:29:53 -0300

    Update ex106.py
    Código base inserido. Tudo funcionando conforme o enunciado. A única diferença que vi em relação ao mostrado no video é que o meu está ficando um espaço (preto) entre as cores de fundo, e quando tento elimiar o espaço, ele não completa a cor até o final, acabando ao termino do texto.

--- Exercicios/ex106.py
+++ Exercicios/ex106.py
@@ -8,15 +8,35 @@ Obs: Use CORES.
 '''
 # ============ CÓDIGO BASE ============================================================================================
 def ajuda(txt):
-    print('\033[47;30m~'*40)
-    print(f'{"SISTEMA DE AJUDA PYTHON":^40}')
-    print('~'*40)
+    from time import sleep
+    while True:
+        print('\033[44;30m~'*40)
+        print(f'{"SISTEMA DE AJUDA PYTHON":^40}')
+        print('~'*40)
 
-    return txt
+        palavra = str(input(f'\n\033[m{txt}').lower())
+
+        if palavra == 'fim':
+            sleep(.5)
+            print('\033[41;30m~'*12)
+            print(f'{"ATÉ LOGO!":^12}')
+            print('~'*12)
+            break
+
+        print('\033[47;30m~'*50)
+        print(f'{"ACESSANDO O MANUAL DO COMANDO " +palavra.upper():^50}')
+        print('~'*50)
+        print('\033[m')
+        sleep(.5)
+
+        print('\033[7;30m')
+        help(palavra)
+        print('\033[m')
+        sleep(2)
+
 
 #Programa principal
-a = ajuda('Função ou Biblíoteca >> ')
-print(a)
+ajuda('Digite a Função ou Comando: >> ')
 # ============ CÓDIGO MELHORADO =======================================================================================
 
 # ============ ANÁLISE DO CÓDIGO ======================================================================================
