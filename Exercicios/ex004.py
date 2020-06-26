c0691f5899f600cec8e6b13f2e7f7a6fcae0f08d
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-26 16:06:29 -0300

    Update ex004.py

--- Exercicios/ex004.py
+++ Exercicios/ex004.py
@@ -6,3 +6,19 @@ E TODAS AS INFORMAÇÕES POSSÍVEIS SOBRE ELE.
 
 Usando as funções .is
 '''
+# ============= CÓDIGO BASE ===========================================================================================
+txt = str(input('Digite algo a sua escolha [Letra ou número]: '))
+print(f'Você digitou {txt} a sua classe é {type(txt)}, segue abaixo as suas caracteristicas:')
+print('-='*30)
+print(f'    --> é alfanumérico (número e/ou texto): {txt.isalnum()}')
+print(f'    --> é alfabeto (apenas texto): {txt.isalpha()}')
+print(f'    --> é {txt.isascii()}')
+print(f'    --> é decimal (apenas número): {txt.isdecimal()}')
+print(f'    --> é um digito apenas númerico: {txt.isdigit()}')
+print(f'    --> é um texto sem espaço: {txt.isidentifier()}')
+print(f'    --> é um texto com todas letras minusculas: {txt.islower()}')
+print(f'    --> é apenas espaço em branco: {txt.isspace()}')
+print(f'    --> é um conteúdo imprimivel (vazio ou texto/números): {txt.isprintable()}')
+print(f'    --> é um texto com a 1ª letra maiúscula: {txt.istitle()}')
+print(f'    --> é um texto com todas letras maiúsculas: {txt.isupper()}')
+
