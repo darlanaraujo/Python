5fb2719ab62159bef4996dc267ec611e159757f5
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-26 16:06:26 -0300

    Update ex003 - Aula 06.py

--- Exercicios/ex003 - Aula 06.py
+++ Exercicios/ex003 - Aula 06.py
@@ -3,3 +3,9 @@ DESAFIO 003
 
 Crie um programa que leia DOIS NÚMEROS E MOSTRE A SOMA ENTRE ELES.
 '''
+# ============== CÓDIGO BASE ==========================================================================================
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+soma = n1 + n2
+print('-='*20)
+print(f'A soma entre {n1} + {n2} é = {soma}')
