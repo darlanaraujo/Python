7b7275152ee81a27bea83571ad91ac0d3ac79702
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-26 16:06:24 -0300

    Update ex002.py

--- Exercicios/ex002.py
+++ Exercicios/ex002.py
@@ -1,6 +1,15 @@
 '''
 DESAFIO 002
 
-Crie um screipt que leia O DIA, MÊS E ANO DE NASCIMENTO de uma pessoa
+Crie um script que leia O DIA, MÊS E ANO DE NASCIMENTO de uma pessoa
 e mostre uma mensagem com a data formatada.
 '''
+# ============ CÓDIGO BASE ============================================================================================
+nome = str(input('Qual o seu nome: ')).title().strip()
+print(f'Olá {nome}, informe abaixo o dia, mês e ano do seu nascimento!')
+dia = int(input('Informe o dia: '))
+mes = str(input('Qual o mês: '))
+ano = int(input('Por fim, qual o ano: '))
+
+print('-='*30)
+print(f'Seu nome é {nome}, e a data de nascimento é {dia}/{mes}/{ano}')
