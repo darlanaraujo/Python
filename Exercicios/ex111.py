f709c59727171c931a386bcff72e30d9ccf5db0a
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-28 21:53:10 -0300

    Create ex111.py
    Enunciado ex111

---
+++ Exercicios/ex111.py
@@ -0,0 +1,7 @@
+'''
+DESAFIO 111
+
+Crie um PACOTE chamado UTILIDADESCEV que tenha dois MÓDULOS INTERNOS chamados MOEDA e DADOS.
+
+Transfira todas as funções utilizadas nos DESAFIOS 107, 108 E 109 para o primeiro pacote e mantenha tudo funcionando.
+'''
