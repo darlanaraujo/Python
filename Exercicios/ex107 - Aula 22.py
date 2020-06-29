dc55850c37a36bfa1ac757058963dfe311354536
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-28 21:52:36 -0300

    Create ex107 - Aula 22.py
    Enunciado ex107

---
+++ Exercicios/ex107 - Aula 22.py
@@ -0,0 +1,7 @@
+'''
+DESAFIO 107
+
+Crie um módulo chamado moedas.py que tenha as funções incorporadas AUMENTAR(), DIMINUIR(), DOBRO() E METADE().
+
+Faça também um programa que IMPORTE esse módulo e use algumas dessas funções.
+'''
