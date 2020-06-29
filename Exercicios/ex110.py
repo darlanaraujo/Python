46ae47b9e160ed8cadf9b69384a9932c0149c9e6
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-28 21:53:02 -0300

    Create ex110.py
    Enunciado ex110

---
+++ Exercicios/ex110.py
@@ -0,0 +1,6 @@
+'''
+DESAFIO 110
+
+Adicione ao módulo moedas.py criado nos desafios anteriores, uma função chamada RESUMO(), que mostre na tela algumas
+informações geradas pelas funções que já temos no módulo criado até aqui.
+'''
