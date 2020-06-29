725f6534ae8a4f6f85a5b9bc66cc4c90c7b4f3be
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-28 21:53:22 -0300

    Create ex112.py
    Enunciado ex112

---
+++ Exercicios/ex112.py
@@ -0,0 +1,7 @@
+'''
+DESAFIO 112
+
+Dentro do pacote UTILIDADESCEV que criamos no DESAFIO 111, temos um MÓDULO chamado DADO. Crie uma função chamada
+LEIADINHEIRO() que seja capaz de funcionar como a função INPUT(), mas com uma VALIDAÇÃO DE DADOS para aceitar apenas
+valores que seja MONETÁRIOS.
+'''
