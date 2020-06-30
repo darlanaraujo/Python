26faa699932a5bd51fdcc12e739f8402c96e0c71
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-29 21:48:15 -0300

    Update ex107 - Aula 22.py
    Código base inserido. Programa funcionando conforme o enunciado.

--- Exercicios/ex107 - Aula 22.py
+++ Exercicios/ex107 - Aula 22.py
@@ -5,3 +5,10 @@ Crie um módulo chamado moedas.py que tenha as funções incorporadas AUMENTAR()
 
 Faça também um programa que IMPORTE esse módulo e use algumas dessas funções.
 '''
+from modulos import moedas
+
+num = float(input('Digite um valor: '))
+print(f'O valor de {num} com aumento de 10% é {moedas.aumentar(num)}')
+print(f'O valor de {num} menos 25% é {moedas.diminuir(num)}')
+print(f'O valor de {num} dobrado é {moedas.dobro(num)}')
+print(f'O valor da metade de {num} é {moedas.metade(num)} ')
