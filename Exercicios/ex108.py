adc297e468275293f5b8b1f5a770a1a9c036fd43
Darlan Araujo <darlanparaujo@outlook.com>
2020-06-29 21:49:11 -0300

    Update ex108.py
    Código base ainda em desenvolvimento

--- Exercicios/ex108.py
+++ Exercicios/ex108.py
@@ -4,3 +4,11 @@ DESAFIO 108
 Adapte o código do DESAFIO 107, criando uma função adicional chamada MOEDA() que consiga mostrar os valores como um
 valor monetário formatado.
 '''
+
+from modulos import moedas
+
+num = int(input('Digite um valor: '))
+print(f'O valor de {num} com aumento de 10% é {moedas.aumentar(num)}')
+print(f'O valor de {num} menos 25% é {moedas.diminuir(num)}')
+print(f'O valor de {num} dobrado é {moedas.dobro(num)}')
+print(f'O valor da metade de {num} é {moedas.metade(num)} ')
