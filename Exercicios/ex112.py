0b1a881ead5d7fe662152ec1a347ad5dc7e96a21
Darlan Araujo <darlanparaujo@outlook.com>
2020-07-03 01:27:34 -0300

    Update ex112.py
    Código base em desenvolvimento. O programa já está funcionanco conforme o enunciado. Porem a duas coisas que não consegui fazer, uma é mostrar na tela a formatação usando virgula ao invés de ponto. Outra é que se o usuário digitar usando ponto, o programa não aceita. Só recenhece se usar numero inteiro, ou a virgula.

--- Exercicios/ex112.py
+++ Exercicios/ex112.py
@@ -5,3 +5,13 @@ Dentro do pacote UTILIDADESCEV que criamos no DESAFIO 111, temos um MÓDULO cham
 LEIADINHEIRO() que seja capaz de funcionar como a função INPUT(), mas com uma VALIDAÇÃO DE DADOS para aceitar apenas
 valores que seja MONETÁRIOS.
 '''
+# ============= CÓDIGO BASE ===========================================================================================
+from modulos.utlidadesCeV import dado, moeda
+
+p = dado.leiadinheiro('Digite um valor: R$ ')
+
+# ============= CÓDIGO APRIMORADO =====================================================================================
+
+# ============= ANÁLISE DO CÓDIGO =====================================================================================
+
+# ============= CÓDIGO CURSO EM VÍDEO =================================================================================
