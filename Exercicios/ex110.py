7ddde8cbeb43e6bb4646ddae7b96772497917aa8
Darlan Araujo <darlanparaujo@outlook.com>
2020-07-03 01:21:54 -0300

    Update ex110.py
    Código base concluido. Tudo funcionando conforme o enunciado. O código funciona atraves do modulo moedas dentro do pacote modulos

--- Exercicios/ex110.py
+++ Exercicios/ex110.py
@@ -4,3 +4,13 @@ DESAFIO 110
 Adicione ao módulo moedas.py criado nos desafios anteriores, uma função chamada RESUMO(), que mostre na tela algumas
 informações geradas pelas funções que já temos no módulo criado até aqui.
 '''
+# ============= CÓDIGO BASE ===========================================================================================
+from modulos import moedas
+
+valor = float(input('Digite o preço: R$ '))
+moedas.resumo(valor, 80, 35)
+# ============= CÓDIGO APRIMORADO =====================================================================================
+
+# ============= ANÁLISE DO CÓDIGO =====================================================================================
+
+# ============= CÓDIGO CURSO EM VÍDEO =================================================================================
