907232751ebe96a5d3088aa5054428e3eb400994
Darlan Araujo <darlanparaujo@outlook.com>
2020-07-05 18:51:54 -0300

    Update ex108.py

--- Exercicios/ex108.py
+++ Exercicios/ex108.py
@@ -7,14 +7,22 @@ valor monetário formatado.
 # ============= CÓDIGO BASE ===========================================================================================
 from modulos import moedas
 
-num = float(input('Digite o preço: R$ '))
+'''num = float(input('Digite o preço: R$ '))
 print(f'O valor de {moedas.moeda(num)} com aumento de 10% é = {moedas.moeda(moedas.aumentar(num, 10))}')
 print(f'O valor de {moedas.moeda(num)} menos 13% é = {moedas.moeda(moedas.diminuir(num, 13))}')
 print(f'O valor de {moedas.moeda(num)} dobrado é = {moedas.moeda(moedas.dobro(num))}')
-print(f'O valor da metade de {moedas.moeda(num)} é = {moedas.moeda(moedas.metade(num))} ')
+print(f'O valor da metade de {moedas.moeda(num)} é = {moedas.moeda(moedas.metade(num))} ')'''
 
 # ============= CÓDIGO APRIMORADO =====================================================================================
 
 # ============= ANÁLISE DO CÓDIGO =====================================================================================
-
+'''
+A criação desse código é bem fácil e ambos os códigos são parecidos. A análise mais detalhada está dentro do módulo
+aonde as funções foram criadas.
+'''
 # ============= CÓDIGO CURSO EM VÍDEO =================================================================================
+
+p = float(input('Digite o preço: R$ '))
+print(f'A metade de {moedas.moeda(p)} é R$ {moedas.moeda(moedas.metade(p))}')
+print(f'O dobro de {moedas.moeda(p)} é R$ {moedas.moeda(moedas.dobro(p))}')
+print(f'Aumentando 10%, temos {moedas.moeda(moedas.aumentar(p, 10))}')
