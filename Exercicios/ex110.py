24169533039193aaa351b6c27f8427a4130e292f
Darlan Araujo <darlanparaujo@outlook.com>
2020-07-05 18:52:06 -0300

    Update ex110.py

--- Exercicios/ex110.py
+++ Exercicios/ex110.py
@@ -7,10 +7,15 @@ informações geradas pelas funções que já temos no módulo criado até aqui.
 # ============= CÓDIGO BASE ===========================================================================================
 from modulos import moedas
 
-valor = float(input('Digite o preço: R$ '))
-moedas.resumo(valor, 80, 35)
+'''valor = float(input('Digite o preço: R$ '))
+moedas.resumo(valor, 80, 35)'''
 # ============= CÓDIGO APRIMORADO =====================================================================================
 
 # ============= ANÁLISE DO CÓDIGO =====================================================================================
-
+'''
+A criação desse código é bem fácil e ambos os códigos são parecidos. A análise mais detalhada está dentro do módulo
+aonde as funções foram criadas.
+'''
 # ============= CÓDIGO CURSO EM VÍDEO =================================================================================
+p = float(input('Digite o preço: R$ '))
+moedas.resumo(p, 80, 35)
