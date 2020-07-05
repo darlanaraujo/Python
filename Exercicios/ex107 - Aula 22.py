a65b4726479985fdf77b400c68193322a6589de5
Darlan Araujo <darlanparaujo@outlook.com>
2020-07-05 18:51:48 -0300

    Update ex107 - Aula 22.py

--- Exercicios/ex107 - Aula 22.py
+++ Exercicios/ex107 - Aula 22.py
@@ -8,14 +8,21 @@ Faça também um programa que IMPORTE esse módulo e use algumas dessas funçõe
 # ============= CÓDIGO BASE ===========================================================================================
 from modulos import moedas
 
-num = float(input('Digite o Preço: R$ '))
+'''num = float(input('Digite o Preço: R$ '))
 print(f'O valor de {num} com aumento de 10% é = {moedas.aumentar(num, 10)}')
 print(f'O valor de {num} menos 13% é = {moedas.diminuir(num, 13)}')
 print(f'O valor de {num} dobrado é = {moedas.dobro(num)}')
-print(f'O valor da metade de {num} é = {moedas.metade(num)} ')
+print(f'O valor da metade de {num} é = {moedas.metade(num)} ')'''
 
 # ============= CÓDIGO APRIMORADO =====================================================================================
 
 # ============= ANÁLISE DO CÓDIGO =====================================================================================
-
+'''
+A criação desse código é bem fácil e ambos os códigos são parecidos. A análise mais detalhada está dentro do módulo
+aonde as funções foram criadas.
+'''
 # ============= CÓDIGO CURSO EM VÍDEO =================================================================================
+p = float(input('Digite o preço: R$ '))
+print(f'A metade de R$ {p} é R$ {moedas.metade(p)}')
+print(f'O dobro de R$ {p} é R$ {moedas.dobro(p)}')
+print(f'Aumentando 10%, temos R$ {moedas.aumentar(p, 10)}')
