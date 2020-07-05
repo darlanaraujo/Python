49a96fb3eb52ee7da39fe76e4e237da4e249b117
Darlan Araujo <darlanparaujo@outlook.com>
2020-07-05 18:52:12 -0300

    Update ex111.py

--- Exercicios/ex111.py
+++ Exercicios/ex111.py
@@ -8,10 +8,15 @@ Transfira todas as funções utilizadas nos DESAFIOS 107, 108 E 109 para o prime
 # ============= CÓDIGO BASE ===========================================================================================
 from modulos.utlidadesCeV import moeda
 
-valor = float(input('Digite o preço: R$ '))
-moeda.resumo(valor, 80, 35)
+'''valor = float(input('Digite o preço: R$ '))
+moeda.resumo(valor, 80, 35)'''
 # ============= CÓDIGO APRIMORADO =====================================================================================
 
 # ============= ANÁLISE DO CÓDIGO =====================================================================================
-
+'''
+A criação desse código é bem fácil e ambos os códigos são parecidos. A análise mais detalhada está dentro do módulo
+aonde as funções foram criadas.
+'''
 # ============= CÓDIGO CURSO EM VÍDEO =================================================================================
+p = float(input('Digite o preço: R$ '))
+moeda.resumo(p, 80, 35)
