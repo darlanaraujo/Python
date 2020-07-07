82d8cb5687d2ba3f185e9f307796140b279b2715
Darlan Araujo <darlanparaujo@outlook.com>
2020-07-07 18:01:07 -0300

    Create aula 23 - Erros e Exceções.py
    Descrição da aula 23 em andamento.

---
+++ Aulas/aula 23 - Erros e Exceções.py
@@ -0,0 +1,78 @@
+'''
+AULA 23 - TRATAMENTO DE ERROS E EXCEÇÕES
+
+O ERRO mais comum é sintaxe - Quando se erra uma palavra.
+Exemplo: primt(x) / input(('texto) / else;
+
+Mas os erros mais cometidos são erros de significado (erro semantico).
+
+Nesse caso o nome usado é EXCEÇÃO
+Exemplo: print(x) # Nesse caso mesmo que o comendo esteja correto, mas, por não ter definido a variável o programa
+daria erro. NameError.
+
+Esse tipo de EXCEÇÃO pode ocorrer de várias maneiras, outro exemplo: n = int(input('Digite um número: '))
+O código está certo, porém, se o usuário digitar um texto ao invés de número, o programa vai dar erro. ValuerError.
+'''
+# ============== ERRO DE SINTAXE ======================================================================================
+# Esse é um erro de sintaxe, aonde o comando foi digitado errado.
+'''primt(x) # comando pint digitado errado.
+num = int(input(('Digite um número: ')) # comando com excesso de parenteses
+
+# ============== EXCEÇÕES =============================================================================================
+# Exceções de erros. Quando o comando está certo, mas a execução da erro.
+
+print(x) #Ainda que o comando esteja correto, não foi definido a variável X. NameError
+
+n = int(input('Digite um número: ')) #O comando está correto, mas se o usuário digitar W. ValuerError.
+
+a = 8
+b = 0
+r = a / b #Esse comando daria erro, devido ao divisor 0. ZeroDivisionError
+
+s = 2 / '2' #Esse comando daria erro uma vez que são tipos diferentes (numeral x texto). TypeError
+
+lst = [9, 2, 4]
+print(lst[3]) #Esse comando daria erro, uma vez que a lista vai de 0 a 2. IndexError
+
+# Outras Exceções: KeyError, EOFError, KeyboardInterrupt, OSError, MemoryError, ConnectionError, RuntimeError
+'''
+# ============== RETORNO QUANDO UM ERRO OU EXCEÇÃO É ENCONTRADA =======================================================
+
+'''
+Traceback (most recent call last):
+  File "C:/Users/Visa Remoções/Desktop/Darlan/Python/Outros/teste 2.py", line 1, in <module>
+    print(x)
+NameError: name 'x' is not defined
+'''
+
+# ============== TRATANDO ERROS E EXCEÇÕES ============================================================================
+
+'''
+No Python e em várias linguagens de programação existe um comando para tratamento de erros e exceções. Com ele você
+"TENTA" executar um comando, e caso não funcione, ela fara outra coisa.
+
+OS COMANDOS SÃO:
+try:
+    Aqui colocamos a operação.
+except:
+    Aqui colocamos o comando que acontece caso haja uma falha.
+else:
+    Aqui colocamos a solução caso o try: funcione.
+    
+O comando try: é para tentar executar algo
+O comando except: é caso dê algum problema e ele pode mostrar outro resultado ou ação.
+Ao colocar o else: com a solução caso o programa não dê erro.
+
+Isso faz com que mesmo tendo algum erro, ao invés de interromper o programa e aparecer a mensagem vermelha. O programa
+vai continuar funcionando (se estiver em um loop) e o programador pode colocar uma mensagem personalizada.
+'''
+
+# AULA PRÁTICA
+try:
+    a = int(input('Denominador: '))
+    b = int(input('Divisor: '))
+    r = a / b
+except:
+    print('Infelizmente tivemos um problema :(')
+else:
+    print(f'O resultado é {r}')
