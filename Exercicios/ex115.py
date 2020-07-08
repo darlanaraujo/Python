d9b8ba0bf3067e2f7bdc2cd098e5a9ddecde152d
Darlan Araujo <darlanparaujo@outlook.com>
2020-07-08 18:01:18 -0300

    Create ex115.py

---
+++ Exercicios/ex115.py
@@ -0,0 +1,8 @@
+'''
+DESAFIO 115
+
+Crie um pequeno SISTEMA MODULARIZADO que permita cadastrar pessoas pelo seu NOME e IDADE em um arquivo de texto
+simples.
+
+O sistema só vai ter 2 OPÇÕES: CADASTRAR uma nova pessoa e LISTAR todas as pessoas cadastradas.
+'''
