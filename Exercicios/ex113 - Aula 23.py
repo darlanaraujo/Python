1ed3abf130010660e53028e313d3846b7c278e9f
Darlan Araujo <darlanparaujo@outlook.com>
2020-07-08 18:01:12 -0300

    Create ex113 - Aula 23.py

---
+++ Exercicios/ex113 - Aula 23.py
@@ -0,0 +1,6 @@
+'''
+DESAFIO 113
+
+Reescreva a função LEIAINT() que fizemos no DESAFIO 104, incluindo agora a possibilidade da digitação de um número
+de tipo inválido. Aproveite e crie também uma função LEIAFLOAT() com a mesma funcionalidade.
+'''
