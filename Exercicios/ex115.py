b41085c71dfb9776d38a1b7383b8447d8480c8a4
Darlan Araujo <darlanparaujo@outlook.com>
2020-07-13 11:54:26 -0300

    Update ex115.py
    Código base em desenvolvimento. Nessa atualização, o código foi retirado do programa e criado um modulo para importação.

--- Exercicios/ex115.py
+++ Exercicios/ex115.py
@@ -7,46 +7,9 @@ simples.
 O sistema só vai ter 2 OPÇÕES: CADASTRAR uma nova pessoa e LISTAR todas as pessoas cadastradas.
 '''
 # ============== CÓDIGO BASE ==========================================================================================
-def menu():
-    print('| A) CADASTRAR')
-    print('| B) CONSULTAR')
-    print('| C) EXCLUIR')
-    print('| D) SAIR DO PROGRAMA')
-    print('|', '-'*50, '|')
-    resp = str(input('| ESCOLHA A OPÇÃO DESEJADA: ')).upper().strip()[0]
-    print('|', '-'*50, '|')
-    if resp == 'A':
-        cadastro()
-    if resp == 'B':
-        print('| Consultar')
-    if resp == 'C':
-        print('| Excluir')
-    if resp == 'D':
-        validacao('Finalizando')
+from modulos import cadastro
 
-
-def cabecalho(msg):
-    print('|', '='*50, '|')
-    print(f'|{msg.center(50)}  |')
-    print('|', '='*50, '|')
-    menu()
-
-
-def validacao(msg):
-    from time import sleep
-    print(f'| {msg}', end='')
-    for c in range(1, 5):
-        sleep(.5)
-        print('.', end='')
-    print('Concluido.')
-
-
-def cadastro():
-    nome = str(input('| Nome completo: ')).strip().upper()
-    validacao('Adicionando')
-
-
-cabecalho('SISTEMA DE CADASTRO')
+cadastro.inicio()
 # ============== CÓDIGO MELHORADO =====================================================================================
 
 # ============== ANÁLISE DO CÓDIGO ====================================================================================
