808a4c91212514c62afaf1fa90a298c3748f0183
Darlan Araujo <darlanparaujo@outlook.com>
2020-07-13 11:55:12 -0300

    Update __init__.py
    Código em desenvolvimento.

---
+++ modulos/cores/__init__.py
@@ -0,0 +1,14 @@
+def cores(cor):
+    bra     = '\033[30m'    # Branco
+    verm    = '\033[31m'    # Vermelho
+    verd    = '\033[32m'    # Verde
+    amar    = '\033[33m'    # Amarelo
+    azul    = '\033[34m'    # Azul
+    roxo    = '\033[35m'    # Roxo
+    clear   = '\033[m'      # Limpa a cor
+
+    cor = bra
+
+
+cores(cor)
+print(f'Cor do texto')
