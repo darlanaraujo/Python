16b5d3cf5caa23e493b292ba4cc33510f118c529
Darlan Araujo <darlanparaujo@outlook.com>
2020-07-14 18:05:17 -0300

    Update ex115.py
    Código do curso em desenvolvimento.

--- Exercicios/ex115.py
+++ Exercicios/ex115.py
@@ -7,11 +7,38 @@ simples.
 O sistema só vai ter 2 OPÇÕES: CADASTRAR uma nova pessoa e LISTAR todas as pessoas cadastradas.
 '''
 # ============== CÓDIGO BASE ==========================================================================================
-from modulos import cadastro
+from modulos.cadastro import *
+from modulos.cadastro.arquivo import *
 
-cadastro.inicio()
+'''inicio()'''
 # ============== CÓDIGO MELHORADO =====================================================================================
 
 # ============== ANÁLISE DO CÓDIGO ====================================================================================
 
 # ============== CÓDIGO CURSO EM VÍDEO ================================================================================
+from time import sleep
+
+arq = 'cursoemvideo.txt'
+
+if not arquivoexiste(arq):
+    criararquivo(arq)
+
+while True:
+    resposta = menu(['Ver Pessoas Cadastradas', 'Cadastrar Nova Pessoa', 'Sair do Sisitema'])
+    if resposta == 1:
+        # Opção de listar o conteúdo de um arquivo.
+        lerarquivo(arq)
+    elif resposta == 2:
+        # Opção de cadastrar uma nova pessoa.
+        cabeçalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaint('Idade: ')
+        cadastrar(arq, nome, idade)
+    elif resposta == 3:
+        # Opção de sair do sistema.
+        cabeçalho('Saindo do sistema... Até logo!')
+        break
+    else:
+        # Digitou uma opção errada no menu.
+        print('\033[31mERRO! Digite uma opção válida!\033[m')
+    sleep(1)
