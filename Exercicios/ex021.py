1a42b7b6709eb3662ae10eae093b918252dfd9dd
Darlan Araujo <darlanparaujo@outlook.com>
2020-07-16 16:35:47 -0300

    Update ex021.py
    Conclusão do ex021

--- Exercicios/ex021.py
+++ Exercicios/ex021.py
@@ -3,3 +3,8 @@ DESAFIO 021
 
 Faça um programa que ABRA E REPRODUZA UM AÚDIO DE UM ARQUIVO MP3.
 '''
+import pygame
+pygame.init()
+pygame.mixer.music.load('abertura.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
