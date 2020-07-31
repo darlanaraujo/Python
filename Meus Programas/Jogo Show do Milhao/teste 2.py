0a63f4ee55acad9c4e3ecdca21d89305db110b22
Darlan Araujo <darlanparaujo@outlook.com>
2020-07-31 17:54:32 -0300

    Update teste 2.py

--- Meus Programas/Jogo Show do Milhao/teste 2.py
+++ Meus Programas/Jogo Show do Milhao/teste 2.py
@@ -75,88 +75,151 @@ pygame.mixer.music.load('audio/frase_dificil_ajuda.mp3')
 pygame.mixer.music.play()
 while (pygame.mixer.music.get_busy()): pass'''
 
-arq = 'perguntas.txt'
 
-perguntas = {
-    'nivel1': [''], 'resposta_nv1': [''],
-    'nivel2': [''], 'resposta_nv2': [''],
-    'nivel3': [''], 'resposta_nv3': [''],
-    'nivel4': [''], 'resposta_nv4': ['']
-}
+def ler_pergunta(arq_pergunta):
+    try:
+        abrir = open(arq_pergunta, 'rt')
+    except:
+        print(f'Houve um ERRO na abertura do aquivo!')
+    else:
+        cabecalho('PERGUNTAS')
+        lista = []
+        temp = []
+
+        for dados in abrir:
+            dado = dados.split(';')
+            dado[5] = dado[5].replace('\n', '')
+
+            temp.append(dado[0])
+            temp.append(dado[1])
+            temp.append(dado[2])
+            temp.append(dado[3])
+            temp.append(dado[4])
+            temp.append(dado[5])
+            lista.append(temp[:])
+            temp.clear()
+
+        sorteio = []
+        while len(sorteio) != len(lista):
+            r = randint(0, len(lista) - 1)
+            if r not in sorteio:
+                sorteio.append(r)
+
+        pergunta = lista[sorteio[0]]
+        resposta = pergunta[5]
+
+        print(
+        f'{fvm}>>>  {pergunta[0]}?   {sc}'
+        f'\n{am}A) {br}{pergunta[1]}'
+        f'\n{am}B) {br}{pergunta[2]}'
+        f'\n{am}C) {br}{pergunta[3]}'
+        f'\n{am}D) {br}{pergunta[4]}')
+        linha()
+
+        resp = str(input('Qual a resposta certa? ')).upper().strip()
+        if resp == resposta:
+            print('Certa Resposta!')
+        else:
+            print('Que pena! Você errou.')
+        linha()
+
+
+    #finally:
+        #abrir.close()
+
+
+def cabecalho(msg):
+    print(br)
+    linha('=')
+    print(f'|{am}{msg.center(78)}{br}|')
+    linha('=')
+
+
+def linha(caractere='-', tam=80):
+    print(caractere * tam)
+
+
+def menu():
+    print('A) CADASTRAR PERGUNTA:')
+    print('B) LISTA PERGUNTA CADASTRADA:')
+
+    while True:
+        opcao = str(input('Escolha a opção desejada: [A/B]: ')).upper().strip()
+        if opcao == 'A':
+            nivel()
+        elif opcao == 'B':
+            global arq_pergunta
+            n = str(input('Qual o nível da pergunta? [1/2/3/4]: ')).lower().strip()
+            arq = f'pergunta_nivel{n}'
+            ler_pergunta(arq_pergunta)
+
 
 def inicio():
+    menu()
+
+
+def nivel():
+    global arq_pergunta
+    cabecalho('CADASTRO DE PERGUNTAS')
+    print(f'Para cadastrar uma pergunta, defina o nivel dela entra as opções {am}1{br} - {am}2{br} - {am}3{br} ou {am}4{br}.')
+    while True:
+        n = str(input('Qual o nível da pergunta? [1/2/3/4]: ')).lower().strip()
+        if n not in '1234':
+            print(f'ERRO! Dados inválidos. Digite apenas {am}1{br} - {am}2{br} - {am}3{br} ou {am}4{br}!')
+        else:
+            arq_pergunta = f'pergunta_nivel{n}'
+            break
+
     # Função para definir o nome do arquivo aonde os dados dos jogadores ficarão salvos.
-    if not arquivoexiste(arq):
-        criararquivo(arq)
+    if not arquivoexiste(arq_pergunta):
+        criararquivo(arq_pergunta)
     else:
-        cadastro_pergunta(arq)
+        cadastro_pergunta(arq_pergunta)
 
 
-def criararquivo(arq):
+def criararquivo(arq_pergunta):
     # Função que cria o arquivo (banco de dados) caso não exista.
     try:
-        a = open(arq, 'wt+')
-        a.close()
+        abrir = open(arq_pergunta, 'wt+')
+        abrir.close()
     except:
-        print(f'Houve um erro na criação do arquivo {arq}')
+        print(f'Houve um erro na criação do arquivo {arq_pergunta}')
     else:
-        print(f'Arquivo {arq} criado com sucesso!')
+        print(f'Arquivo {arq_pergunta} criado com sucesso!')
+        cadastro_pergunta(arq_pergunta)
 
 
-def arquivoexiste(arq):
+def arquivoexiste(arq_pergunta):
     # Função que verifica se o arquivo (banco de dados) existe.
     try:
-        a = open(arq, 'rt')
+        abrir = open(arq_pergunta, 'rt')
+        abrir.close()
     except FileNotFoundError:
         return False
     else:
         return True
 
 
-def cadastro_pergunta(arq):
+def cadastro_pergunta(arq_pergunta):
     while True:
-        nivel = str(input('Qual o nível da pergunta? [1/2/3/4]: ')).lower().strip()
         pergunta = str(input('Qual a Pergunta: ')).title().strip()
         a = str(input('Opção A: ')).upper().strip()
         b = str(input('Opção B: ')).upper().strip()
         c = str(input('Opção C: ')).upper().strip()
         d = str(input('Opção D: ')).upper().strip()
-        resposta = str(input('Qual a resposta certa? [A/B/C/D]: '))
+        resposta = str(input('Qual a resposta certa? [A/B/C/D]: ')).upper().strip()
 
         try:
-            a = open(arq, 'at')
+            abrir = open(arq_pergunta, 'at')
         except:
             print('Houve um ERRO na abertura do arquivo!')
         else:
-            lista = []
-            '''lista.append(
-                f'{fvm}>>>  {pergunta}?   {sc}'
-                f'\n{am}A) {br}{a}'
-                f'\n{am}B) {br}{b}'
-                f'\n{am}C) {br}{c}'
-                f'\n{am}D) {br}{d}',
-            )'''
-
-            lista.append(
-                f'>>> {pergunta}? '
-                f'\nA) {a}'
-                f'\nB) {b}'
-                f'\nC) {c}'
-                f'\nD) {d}',
-            )
-
-            perguntas[f'nivel{nivel}'] = lista
-            perguntas[f'resposta_nv{nivel}'] = resposta
-
             try:
-                a.write(perguntas[f'nival{nivel}'])
-                #a.write(perguntas[f'resposta_nv{nivel}'])
+                abrir.write(f'{pergunta};{a};{b};{c};{d};{resposta}\n')
             except:
                 print(f'ERRO ao cadastrar a pergunta')
             else:
-                a.close()
-
-        #lista.clear()
+                abrir.close()
 
         print('Adicionando pergunta', end='')
         for c in range(1, 6):
@@ -169,6 +232,7 @@ def cadastro_pergunta(arq):
         if resp == 'S':
             continue
         elif resp == 'N':
+            ler_pergunta(arq_pergunta)
             break
         else:
             print('ERRO! Dados inválidos, digite apenas S ou N.')
@@ -177,9 +241,11 @@ def cadastro_pergunta(arq):
 inicio()
 
 '''sorteio = []
-while len(sorteio) != len(nivel1['pergunta']):
-    r = randint(0, len(nivel1['pergunta']) - 1)
+while len(sorteio) != len(dado):
+    r = randint(0, len(dado) -1)
     if r not in sorteio:
-        sorteio.append(r)'''
+        sorteio.append(r)
 
-print(perguntas['nivel1'])
+print(sorteio)'''
+
+
